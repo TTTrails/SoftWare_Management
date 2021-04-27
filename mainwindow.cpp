0bdb129d79ba8c6c612dc38be424f0f91bb9770a
Xuduowei <xdw550184692@gmail.com>
2021-04-27 11:00:57 +0800

    画图

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,34 +8,68 @@ MainWindow::MainWindow(QWidget *parent)
     , isPicOpen(false)
 {
     ui->setupUi(this);
+
+
+
+    //myPaint=new MyPaint(ui->frame);
+    myPaint=new MyPaint(ui->centralwidget);
+    //myPaint->stackUnder(ui->frame);
+    //这都是写死了的，实在不明智
+    //myPaint->setGeometry(QRect(-1, -1, 511, 561));
+    myPaint->setGeometry(QRect(9, 79, 511, 561));
+
+    //myPaint->setFrameShape(QFrame::Box);
+    //myPaint->setFrameShadow(QFrame::Sunken);
+
+
+    rMenu = new QMenu(this);//创建右键菜单
+    //rMenu->addAction(tr("保存  \tCtrl+S"), this, SLOT(SavePic()));//添加菜单动作
+    rMenu->addAction(tr("退出  \tALT+F4"), this, SLOT(close()));//添加菜单动作
+    rMenu->setStyleSheet("background-color:rgb(199,237,204)");//设置背景色
+
+
+    //连接信号与槽函数
+    connect(ui->action_draw_pencil, SIGNAL(triggered()), myPaint, SLOT(Lines()));
+    connect(ui->action_draw_rectangle, SIGNAL(triggered()), myPaint, SLOT(Rects()));
+    connect(ui->action_draw_ellipse, SIGNAL(triggered()), myPaint, SLOT(Ellipses()));
+    connect(ui->action_draw_line, SIGNAL(triggered()), myPaint, SLOT(Line()));
+    connect(ui->action_draw_text, SIGNAL(triggered()), myPaint, SLOT(Texts()));
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
-int MainWindow::YUVConvertToRGB(){
-    unsigned char* data =curImage.bits();
-    unsigned char* RGBData;
-    int width=curImage.width();
-    int height=curImage.height();
-    int bytePerLine=(width*24+31)/8;
-    //存储处理后的数据
-    RGBData=new unsigned char [bytePerLine*height];
-    //unsigned char r,g,b,y;
 
-    for(int i=0;i<height;i++)
-    {
-        for(int j=0;j<width;j++)
-        {
-
-        }
-    }
-    QImage RGBImage(RGBData,width,height,bytePerLine,QImage::Format_RGB888);
-    curImage=RGBImage;
-
-    return 0;
+void MainWindow::contextMenuEvent(QContextMenuEvent *)  //右键菜单事件
+{
+    rMenu->exec(cursor().pos());//在光标位置弹出菜单
 }
+
+//int MainWindow::YUVConvertToRGB(){
+//    unsigned char* data =curImage.bits();
+//    unsigned char* RGBData;
+//    int width=curImage.width();
+//    int height=curImage.height();
+//    int bytePerLine=(width*24+31)/8;
+//    //存储处理后的数据
+//    RGBData=new unsigned char [bytePerLine*height];
+//    //unsigned char r,g,b,y;
+
+//    for(int i=0;i<height;i++)
+//    {
+//        for(int j=0;j<width;j++)
+//        {
+
+//        }
+//    }
+//    QImage RGBImage(RGBData,width,height,bytePerLine,QImage::Format_RGB888);
+//    curImage=RGBImage;
+
+//    return 0;
+//}
 
 int MainWindow::RGBConvertToYUV(){
     unsigned char* data =curImage.bits();
@@ -72,18 +106,42 @@ int MainWindow::RGBConvertToYUV(){
 
 
 void MainWindow::paintEvent(QPaintEvent *){
+
+//    QPixmap pix;
+//    pix.load(filePath);//加载图片
+//    QPainter painter(&myPaint->_pixmap);
+
     //1 创建画家对象
-    QPainter painter(this);
+    //QPainter painter(this);
+    QPainter painter(myPaint);
     //2 获取绘图所在矩形区域，当前是在frame窗口
 
-    QRect rect = ui->frame->frameRect();
-
+    //QRect rect = ui->frame->frameRect();
+    QRect rect=myPaint->rect();
     //坐标值平移，让rect和painter使用相同的坐标系,在这个ui设计中会有很多的问题
-    rect.translate(ui->centralwidget->pos()+ui->frame->pos());
+    //rect.translate(ui->centralwidget->pos()+ui->frame->pos());
 
 
     //使用painter将image图片画到rect
+    //painter.drawImage(rect,curImage);
     painter.drawImage(rect,curImage);
+
+
+
+
+
+
+
+//    //试一试在frame_draw上画
+//    QPainter painter_draw(myPaint);
+//    //QImage 绘图
+//    QImage img(300,300,QImage::Format_RGB32);
+//    img.fill(Qt::white);
+//    painter.setPen(QPen(Qt::blue));
+//    painter.drawRect(myPaint->rect());
+
+
+
 
 }
 
@@ -232,6 +290,33 @@ void MainWindow::on_action_histogram_equalization_triggered()
 {
     RGBConvertToYUV();
     update();
+}
+
+
+//画图方面咯
+void MainWindow::on_action_draw_pencil_triggered()
+{
+
+}
+
+void MainWindow::on_action_draw_line_triggered()
+{
+
+}
+
+void MainWindow::on_action_draw_ellipse_triggered()
+{
+
+}
+
+void MainWindow::on_action_draw_rectangle_triggered()
+{
+
+}
+
+void MainWindow::on_action_draw_text_triggered()
+{
+
 }
 
 
--- mainwindow.h
+++ mainwindow.h
@@ -8,7 +8,8 @@
 #include <QDebug>
 #include <QMessageBox>
 #include <QPixmap>
-
+#include <QTextEdit>
+#include "mypaint.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -37,10 +38,24 @@ private slots:
 
     void on_action_inverse_color_triggered();
 
+    void on_action_draw_pencil_triggered();
+
+    void on_action_draw_line_triggered();
+
+    void on_action_draw_ellipse_triggered();
+
+    void on_action_draw_rectangle_triggered();
+
+    void on_action_draw_text_triggered();
+
+    //void AddTexts();//添加文字
+
 private:
 //绘图事件处理，虚函数
-    void paintEvent(QPaintEvent*);
+    void paintEvent(QPaintEvent*);//重写窗体重绘事件
 
+
+    void contextMenuEvent(QContextMenuEvent *);//重写菜单事件
 private:
     Ui::MainWindow *ui;
 //文件路径
@@ -51,5 +66,12 @@ private:
     QRect curRect;
 //显示的Qimage对象
     QImage curImage;
+//右键菜单
+    QMenu * rMenu;
+
+//绘图类
+    MyPaint* myPaint;
+
+
 };
 #endif // MAINWINDOW_H
