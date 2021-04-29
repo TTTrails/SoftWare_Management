0f18a4e6b0c86372c11a44ecd80c0967e20313fd
Xuduowei <xdw550184692@gmail.com>
2021-04-29 14:05:07 +0800

    Squashed commit of the following:
    commit 8ffd13289d980b7415b344b4178a955d0896cfe2
    Merge: 91693fd f09c416
    Author: Xuduowei <xdw550184692@gmail.com>
    Date:   Thu Apr 29 14:02:29 2021 +0800
    
        Merge branch 'main' of github.com:TTTrails/littlePS into xdw
    
        merge
    
    commit 91693fd8890f2088776df1b62b0d881c6d0fdbf6
    Author: Xuduowei <xdw550184692@gmail.com>
    Date:   Thu Apr 29 13:53:46 2021 +0800
    
        合并分支
    
    commit 7e79c52127c355df75d024d3a204cf3d98db70c0
    Merge: d22c7b7 d86bb3f
    Author: Xuduowei <xdw550184692@gmail.com>
    Date:   Wed Apr 28 22:01:41 2021 +0800
    
        Merge branch 'zyk' of github.com:TTTrails/littlePS into xdw
        合并分支
    
    commit d22c7b73140d77a70509c82575ded228b0ce60e1
    Author: Xuduowei <xdw550184692@gmail.com>
    Date:   Wed Apr 28 22:01:17 2021 +0800
    
        merge zky to main and create new branch xdw
    
    commit d86bb3f23407d1de82ddf6277415cde9222eefed
    Merge: 22afa20 f9b35b2
    Author: muluoleiguo <45161141+muluoleiguo@users.noreply.github.com>
    Date:   Wed Apr 28 21:40:39 2021 +0800
    
        Merge branch 'main' into zyk
    
    commit 22afa201732b33666ca67f92da32042ffa2e6cde
    Author: zyk1414 <1445061374@qq.com>
    Date:   Tue Apr 27 15:53:09 2021 +0800
    
        ImageLayer1.0
    
    merge

---
+++ ImageLayerManager.cpp
@@ -0,0 +1,129 @@
+#include"ImageLayerManager.h"
+void ImageLayerManager::sortLayers(){
+    ImageLayer* temp;
+    for (int i = 0; i < layers->size()-1; i++) {
+        for (int j = 0; j < layers->size() -1- i; j++) {
+            if ((layers->at(j)->getPriority() > layers->at(j + 1)->getPriority())) {
+                temp = (*layers)[j];
+                (*layers)[j] = (*layers)[j+1];
+                (*layers)[j+1] = temp;
+            }
+        }
+    }
+}
+QImage ImageLayerManager::BlendAllLayers(){
+    if(isEmpty())
+        return QImage();
+
+    QImage oldImg1,oldImg2;
+    oldImg1=layers->at(0)->getMainImage();
+    //qDebug()<<"oldImg Size:"<<oldImg1.size();
+    if(layers->size()==1){
+        return oldImg1;
+    }
+    else{
+        sortLayers();
+        qDebug()<<layers->at(0)->getLayerName();
+        qDebug()<<layers->at(0)->getMainImage();
+        return oldImg1;
+//        for(int i=1;i<layers->size();i++){
+//            QImage oldImg2=layers->at(i)->getMainImage();
+//            QImage *newImage = new QImage(oldImg1.width(), oldImg1.height(), QImage::Format_ARGB32);
+//            newImage->fill(0);
+//            QColor oldColor1,oldColor2;
+//            int r,g,b;
+////混合图层方式
+//            for(int x=0; x<newImage->width(); x++){
+//                for(int y=0; y<newImage->height(); y++){
+//                    oldColor1 = QColor(oldImg1.pixel(x,y));
+//                    oldColor2 = QColor(oldImg2.pixel(x,y));
+//                    r = (oldColor1.red() +  oldColor2.red())\2;
+//                    g = (oldColor1.green() +  oldColor2.green())\2;
+//                    b = (oldColor1.blue() +  oldColor2.blue())\2;
+
+//                    //we check if the new values are between 0 and 255
+
+//                    r = qBound(0, r, 255);
+//                    g = qBound(0, g, 255);
+//                    b = qBound(0, g, 255);
+
+//                    newImage->setPixel(x,y, qRgb(r,g,b));
+//              }
+//            }
+//            oldImg1=*newImage;
+//       }
+//        qDebug()<<layers->at(0)->getLayerName();
+//        return oldImg1;
+    }
+}
+ImageLayer* ImageLayerManager::getLayer(QString LayerName)
+{
+    if(isEmpty())
+        return NULL;
+    for(auto layer=layers->begin();layer!=layers->end();layer++)
+        if((*layer)->getLayerName()==LayerName){
+            return (*layer);
+        }
+    return NULL;
+}
+
+ImageLayer* ImageLayerManager::getLayer(int priority)
+{
+    if(isEmpty())
+        return NULL;
+    for(auto layer=layers->begin();layer!=layers->end();layer++)
+        if((*layer)->getPriority()==priority){
+            return (*layer);
+        }
+    return NULL;
+}
+
+
+void ImageLayerManager::AddLayer(ImageLayer* layer)
+{
+    layers->append(layer);
+}
+ImageLayer* ImageLayerManager::AddLayer(QImage img)
+{
+    ImageLayer* newLayer=ImageLayerFactory::getImageLayerFactory().ProductImageLayer(img);
+    layers->append(newLayer);
+    return newLayer;
+}
+void ImageLayerManager::RemoveLayer(int priority)
+{
+    if(isEmpty())
+        return;
+    for(auto i=layers->begin();i!=layers->end();i++ ){
+        if((*i)->getPriority()==priority){
+            layers->erase(i);
+        }
+    }
+}
+
+void ImageLayerManager::RemoveLayer(QString Layername)
+{
+    if(isEmpty())
+        return;
+    for(auto i=layers->begin();i!=layers->end();i++ ){
+        if((*i)->getLayerName()==Layername){
+            layers->erase(i);
+        }
+    }
+}
+
+bool ImageLayerManager::isEmpty()
+{
+    if(layers->empty())
+        return true;
+    else
+        return false;
+}
+
+void ImageLayerManager::DebugOut()
+{
+    if(isEmpty())
+        qDebug()<<"layer is empty";
+    for(auto i=layers->begin();i!=layers->end();i++ )
+        qDebug()<<(*i)->getLayerName()<<(*i)->getPriority();
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -1,6 +1,6 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include"ImageLayerObserver.h"
 #include <QMainWindow>
 #include <QDialog>
 #include <QFileDialog>
@@ -9,7 +9,8 @@
 #include <QMessageBox>
 #include <QPixmap>
 #include <QTextEdit>
-#include "mypaint.h"
+#include <QLabel>
+#include <QMouseEvent>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -26,6 +27,9 @@ public:
 //RGB to YUV
     int RGBConvertToYUV();
     int YUVConvertToRGB();
+
+
+    void GenerateDrawImage();
 //自动生成的槽函数
 private slots:
     void on_action_openPic_triggered();
@@ -48,30 +52,81 @@ private slots:
 
     void on_action_draw_text_triggered();
 
-    //void AddTexts();//添加文字
+    void AddTexts();//添加文字
 
-private:
-//绘图事件处理，虚函数
+
+    //zyk
+    void on_button_upLayer_clicked();
+
+    void on_button_downLayer_clicked();
+
+    void on_button_removeLayer_clicked();
+
+
+protected:
+//绘图事件,鼠标事件，菜单事件
     void paintEvent(QPaintEvent*);//重写窗体重绘事件
 
+    void mousePressEvent(QMouseEvent* event);
+    void mouseReleaseEvent(QMouseEvent *);//重写鼠标释放事件
+    void mouseMoveEvent(QMouseEvent *);//重写鼠标移动事件
 
     void contextMenuEvent(QContextMenuEvent *);//重写菜单事件
+
+    bool eventFilter(QObject* watched,QEvent* event);
+
+
 private:
     Ui::MainWindow *ui;
 //文件路径
     QString filePath;
 //是否打开图片
     bool isPicOpen;
-//rect 区域
+
+
+    //zyk
+    ImageLayerObserver* layerViewModel;
+
+    //rect 区域
     QRect curRect;
-//显示的Qimage对象
+    //显示的Qimage对象
     QImage curImage;
-//右键菜单
+    //右键菜单
     QMenu * rMenu;
 
-//绘图类
-    MyPaint* myPaint;
+    //画图相关
 
+    int lpress;//左键按下标志
+    int drag;//拖拽标志
+    int drawType;//描绘类型
+    int openflag;//打开图片
+    QTextEdit* tEdit;//初始化文本输入框
+
+public:
+    QPixmap pixmap;//画布图片
+
+    QVector<QVector<QPoint> > lines;//线条集合(一条线条可包含多个线段)
+    QVector<QRect> rects;//矩形集合
+    QVector<QRect> ellipse;//椭圆集合
+    QVector<QRect>  line;//直线集合
+    QVector<QString>  text;//文字集合
+    QVector<QPoint>  tpoint;//文字位置集合
+    QVector<int>  shape;//图形类型集合，用于撤回功能
+    QPoint begin;//鼠标按下坐标、用于最后一个图形移动
+
+
+
+
+
+
+signals:
+    void AddNewLayer(QImage img);
+    void listUpdate(QListWidget* list);
+private:
+    void ShowBlendingImage();
+    void AddItemInList(ImageLayer* layer);
+    void RemoveLayer();
+    QListWidgetItem* CopyItem(QListWidgetItem *layer,int index);
 
 };
 #endif // MAINWINDOW_H
